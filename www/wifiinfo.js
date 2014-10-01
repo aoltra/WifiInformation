20b8a12163352d28658c4d4e5444927f7bba0097
Alfredo Oltra <aoltra@gmail.com>
2014-10-01 17:55:26 +0200

    Exposición de la función getSSID

--- www/wifiinfo.js
+++ www/wifiinfo.js
@@ -1,15 +1,22 @@
-var wifiinfo = {  
+var wifiinfo = {
   
-  getBSSID: function(successCallback, errorCallback){  
-    cordova.exec(successCallback,  
-        errorCallback,  
-        "WifiInformation",  
-        "getBSSID",
-        "getSSID",
-        []
-    );  
-      
-  }  
-}  
+    getBSSID: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, 
+            errorCallback,
+            "WifiInformation",
+            "getBSSID",
+            []
+        );
+    },
+    
+    getSSID: function (successCallback, errorCallback) {
+        cordova.exec(successCallback,
+            errorCallback,
+            "WifiInformation",
+            "getSSID",
+            []
+        ); 
+    }
+};
 
 module.exports = wifiinfo;
